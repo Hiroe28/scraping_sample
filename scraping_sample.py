ac1165a883a13f06ba4447a1a4a52398ab5b3667
Hiroe <130797626+Hiroe28@users.noreply.github.com>
2023-08-28 17:44:28 +0900

    Add files via upload

---
+++ scraping_sample.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_wikipedia_article(keyword):
+    # WikipediaのURLを生成
+    url = f"https://ja.wikipedia.org/wiki/{keyword}"
+    
+    # ページのデータを取得
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        return "記事が見つかりません"
+
+    # HTMLを解析
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # タイトルと最初の3つのパラグラフを取得
+    title = soup.find('h1', {'id': 'firstHeading'}).text
+    paragraphs = soup.find_all('p', limit=3)
+    intro_text = ' '.join([p.text for p in paragraphs])
+
+    return title, intro_text
+
+# キーワードを指定（例: "Python"）
+keyword = "Python"
+
+title, intro_text = get_wikipedia_article(keyword)
+
+print("タイトル:", title)
+print("最初の3つのパラグラフ:", intro_text)
