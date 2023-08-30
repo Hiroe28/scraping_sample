249bdee074f28a309b58b96e6088d802e9a4b6b0
Hiroe <130797626+Hiroe28@users.noreply.github.com>
2023-08-31 06:34:41 +0900

    Update scr_ex1.py

--- scr_ex1.py
+++ scr_ex1.py
@@ -1,24 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_wikipedia_article(keyword):
     # WikipediaのURLを生成
     url = f"https://ja.wikipedia.org/wiki/{keyword}"
 
     # User-Agent ヘッダーを設定
     headers = {
-        'User-Agent': 'EducationalResearch; PythonTutorial/1.0; Contact: YourEmail@example.com'
+        'User-Agent': (
+            'EducationalResearch; PythonTutorial/1.0; '
+            'Contact: YourEmail@example.com'
+        )
     }
-    
+
     # ページのデータを取得
-    response = requests.get(url)
-    
+    response = requests.get(url, headers=headers)
+
     if response.status_code != 200:
         return "記事が見つかりません"
 
     # HTMLを解析
     soup = BeautifulSoup(response.content, 'html.parser')
-    
+
     # タイトルと最初の3つのパラグラフを取得
     title = soup.find('h1', {'id': 'firstHeading'}).text
     paragraphs = soup.find_all('p', limit=3)
@@ -26,10 +30,12 @@ def get_wikipedia_article(keyword):
 
     return title, intro_text
 
+
 # キーワードを指定（例: "Python"）
 keyword = "Python"
 
 title, intro_text = get_wikipedia_article(keyword)
+
 
 print("タイトル:", title)
 print("最初の3つのパラグラフ:", intro_text)
